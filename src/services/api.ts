cd31c003124bb75797e26fd8ac5620297790b512
somnath-chattaraj <somnathchattaraj5@gmail.com>
2025-06-15 00:23:42 +0530

    Update backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Student, Contest, Submission, ProblemStats } from '@/types/student';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://project-pry1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
